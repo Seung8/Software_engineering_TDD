7f5099428e57a4deeb6d55640ca9c261be6069a0
seung8 <lsy0914@naver.com>
2017-07-27 13:49:50 +0900

    second test finish

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -3,9 +3,11 @@ public class Dollar {
 
 	public Dollar(int dollar){
 		// 클래스명과 같은 public 변수는 생성자 (클래스와 이름이 같다)
+		amount = dollar;
 	}
 	public void times(int multiplier){
 		// void 는 리턴 값이 없음을 뜻함
+		amount = amount * multiplier;
 
 	}
 }
--- src/test/java/TestCurrency.java
+++ src/test/java/TestCurrency.java
@@ -16,7 +16,7 @@ public class TestCurrency {
 	@Test
 	public void testMultiplication2() {
 		Dollar five = new Dollar(5);
-		five.times(3)
-		assertEquals(10, five.amount);
+		five.times(3);
+		assertEquals(15, five.amount);
 	}
 }
