30a2e1d9ade2382adb168bbf37379027e0682ac8
seung8 <lsy0914@naver.com>
2017-07-27 14:32:16 +0900

    add two five test

--- src/test/java/TestCurrency.java
+++ src/test/java/TestCurrency.java
@@ -9,7 +9,7 @@ public class TestCurrency {
 		Dollar five = new Dollar(5);
 		// 5의 값을 가진 Dollar 객체 생성 후 five 변수에 할당
 		five.times(2);
-		// five 변수 2배
+		// five 변수 2배 ㄱ
 		assertEquals(10, five.amount);
 	}
 
@@ -17,6 +17,17 @@ public class TestCurrency {
 	public void testMultiplication2() {
 		Dollar five = new Dollar(5);
 		five.times(3);
+		// five 변수 3배 ㄱ
 		assertEquals(15, five.amount);
+	}
+
+	@Test
+	public void testMultiplication3() {
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+		five.times(3);
+		assertEquals(15, five.amount);
+
 	}
 }
