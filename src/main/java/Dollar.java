8d65e649f9d943833c92985b0d3bcf69237c2170
seung8 <lsy0914@naver.com>
2017-07-27 14:33:30 +0900

    add two five test finish

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,13 +1,15 @@
 public class Dollar {
+	public int original;
 	public int amount = 10;
 
 	public Dollar(int dollar){
 		// 클래스명과 같은 public 변수는 생성자 (클래스와 이름이 같다)
 		amount = dollar;
+		original = dollar;
 	}
 	public void times(int multiplier){
 		// void 는 리턴 값이 없음을 뜻함
-		amount = amount * multiplier;
+		amount = original * multiplier;
 
 	}
 }
